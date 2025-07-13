134ed710a986945e673439b16f9c36d8f003d20e
David <dmgorraiz@gmail.com>
2025-07-13 13:34:50 -0500

    Ultima version de la API con spring, tiene una capa de dominio que permite la integracion con cualquier base de datos solo requiere que se mapeen los objetos de dominio con los de persistencia que pertenecen a la base de datos y tambien tiene documentación en swagger

---
+++ build/generated/sources/annotationProcessor/java/main/com/david/market/persistence/mapper/CategoryMapperImpl.java
@@ -0,0 +1,49 @@
+package com.david.market.persistence.mapper;
+
+import com.david.market.domain.Category;
+import com.david.market.persistence.entity.Categoria;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-07-09T18:46:30-0500",
+    comments = "version: 1.6.3, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.14.2.jar, environment: Java 17.0.8.1 (Amazon.com Inc.)"
+)
+@Component
+public class CategoryMapperImpl implements CategoryMapper {
+
+    @Override
+    public Category toCategory(Categoria categoria) {
+        if ( categoria == null ) {
+            return null;
+        }
+
+        Category category = new Category();
+
+        if ( categoria.getIdCategoria() != null ) {
+            category.setCategoryId( categoria.getIdCategoria() );
+        }
+        category.setCategory( categoria.getDescripcion() );
+        if ( categoria.getEstado() != null ) {
+            category.setActive( categoria.getEstado() );
+        }
+
+        return category;
+    }
+
+    @Override
+    public Categoria toCategoria(Category category) {
+        if ( category == null ) {
+            return null;
+        }
+
+        Categoria categoria = new Categoria();
+
+        categoria.setIdCategoria( category.getCategoryId() );
+        categoria.setDescripcion( category.getCategory() );
+        categoria.setEstado( category.isActive() );
+
+        return categoria;
+    }
+}
---
+++ build/generated/sources/annotationProcessor/java/main/com/david/market/persistence/mapper/PurchaseItemMapperImpl.java
@@ -0,0 +1,77 @@
+package com.david.market.persistence.mapper;
+
+import com.david.market.domain.PurchaseItem;
+import com.david.market.persistence.entity.ComprasProducto;
+import com.david.market.persistence.entity.ComprasProductoPK;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-07-09T18:46:30-0500",
+    comments = "version: 1.6.3, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.14.2.jar, environment: Java 17.0.8.1 (Amazon.com Inc.)"
+)
+@Component
+public class PurchaseItemMapperImpl implements PurchaseItemMapper {
+
+    @Override
+    public PurchaseItem toPurchaseItem(ComprasProducto producto) {
+        if ( producto == null ) {
+            return null;
+        }
+
+        PurchaseItem purchaseItem = new PurchaseItem();
+
+        Integer idProducto = productoIdIdProducto( producto );
+        if ( idProducto != null ) {
+            purchaseItem.setProductId( idProducto );
+        }
+        if ( producto.getCantidad() != null ) {
+            purchaseItem.setQuantity( producto.getCantidad() );
+        }
+        if ( producto.getEstado() != null ) {
+            purchaseItem.setActive( producto.getEstado() );
+        }
+        if ( producto.getTotal() != null ) {
+            purchaseItem.setTotal( producto.getTotal() );
+        }
+
+        return purchaseItem;
+    }
+
+    @Override
+    public ComprasProducto toComprasProducto(PurchaseItem item) {
+        if ( item == null ) {
+            return null;
+        }
+
+        ComprasProducto comprasProducto = new ComprasProducto();
+
+        comprasProducto.setId( purchaseItemToComprasProductoPK( item ) );
+        comprasProducto.setCantidad( item.getQuantity() );
+        comprasProducto.setEstado( item.isActive() );
+        comprasProducto.setTotal( item.getTotal() );
+
+        return comprasProducto;
+    }
+
+    private Integer productoIdIdProducto(ComprasProducto comprasProducto) {
+        ComprasProductoPK id = comprasProducto.getId();
+        if ( id == null ) {
+            return null;
+        }
+        return id.getIdProducto();
+    }
+
+    protected ComprasProductoPK purchaseItemToComprasProductoPK(PurchaseItem purchaseItem) {
+        if ( purchaseItem == null ) {
+            return null;
+        }
+
+        ComprasProductoPK comprasProductoPK = new ComprasProductoPK();
+
+        comprasProductoPK.setIdProducto( purchaseItem.getProductId() );
+
+        return comprasProductoPK;
+    }
+}
